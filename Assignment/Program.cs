7b8d0345800262d604d4a6941a58b090a8deb666
atlants1998 <atlants.98@gmail.com>
2024-12-17 02:15:27 +0200

    add assignment05

---
+++ Assignment/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        // Function to calculate the sum of digits
+        static int SumOfDigits(int number)
+        {
+            int sum = 0;
+
+            // Loop through each digit of the number
+            while (number > 0)
+            {
+                sum += number % 10;  // Add the last digit to the sum
+                number /= 10;         // Remove the last digit
+            }
+
+            return sum;
+        }
+
+        // Function to check if a number is prime
+        static bool IsPrime(int number)
+        {
+            // A number less than or equal to 1 is not prime
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            // Check divisibility from 2 to the square root of the number
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)  // If divisible by any number in this range, it's not prime
+                {
+                    return false;
+                }
+            }
+
+            // If no divisors were found, the number is prime
+            return true;
+        }
+
+        // Function to find the minimum and maximum values in an array using reference parameters
+        static void MinMaxArray(int[] array, ref int minValue, ref int maxValue)
+        {
+            // Check if the array is empty
+            if (array.Length == 0)
+            {
+                Console.WriteLine("Array is empty!");
+                return;
+            }
+
+            // Initialize min and max values with the first element of the array
+            minValue = array[0];
+            maxValue = array[0];
+
+            // Loop through the array to find the min and max
+            foreach (int value in array)
+            {
+                if (value < minValue)
+                {
+                    minValue = value;
+                }
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            /*Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+        Output should be like 
+        Enter a number: 25 */
+
+            //Console.Write("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int result = SumOfDigits(number);
+            //Console.WriteLine($"The sum of the digits of {number} is: {result}");
+
+            /*Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:*/
+            //Console.Write("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (IsPrime(number))
+            //{
+            //    Console.WriteLine($"{number} is a prime number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{number} is not a prime number.");
+            //}
+
+            /*Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters*/
+            //int[] numbers = { 5, 2, 9, 3, 7, 1, 8 };
+
+            //int min = 0;
+            //int max = 0;
+
+            //MinMaxArray(numbers, ref min, ref max);
+
+            //Console.WriteLine($"The minimum value is: {min}");
+            //Console.WriteLine($"The maximum value is: {max}");
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        #region Swap
+        // Class Member Function (Static)
+        public static void Swap( int X,  int Y) {
+            Console.WriteLine($" Func Start \n ***********");
+            int Swap = X;
+            X = Y;
+            Y = Swap;
+            Console.WriteLine(X);
+            Console.WriteLine(Y);
+            Console.WriteLine($"Func END \n ***********");
+        }
+
+        public static void SwapByRef(ref int X, ref int Y)
+        {
+
+            Console.WriteLine($" Func Start \n ***********");
+            Console.WriteLine($"{X} - {X.GetHashCode()}");
+            Console.WriteLine($"{Y} - {Y.GetHashCode()}");
+            int Swap = X;
+            X = Y;
+            Y = Swap;
+            Console.WriteLine($"{X} - {X.GetHashCode()}");
+            Console.WriteLine($"{Y} - {Y.GetHashCode()}");
+            Console.WriteLine($"Func END \n ***********");
+        }
+
+        public static void SumMul(int X , int Y , out int Sum , out int Mul) {
+            Sum = X+Y;
+            Mul = X*Y;
+        }
+
+        public static int SumArray(params int[] Arr)
+        //params- tatata- pramms mama
+        {
+            int Sum = 0;
+            if (Arr is not null) {
+                for (int i = 0; i < Arr.Length; i++) {
+                    Sum += Arr[i];// Sum = Sum + Arr[i]
+                }
+            }
+            return Sum;
+
+        }
+
+        // Opject Member Function (OOP)
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Demo 05!");
+
+            #region Judged Array
+            //// One D Array Of Arrats With Diffrent Sizes
+
+            //int[][] Numbers = new int[3][];
+            //Numbers[0] =  new int[] { 1,2,3 };
+            //Numbers[1] = new int[] { 4,5 };
+            //Numbers[2] = new int[] {6};
+
+            //for (int i = 0; i < Numbers.Length; i++) {
+            //    for (int j = 0; j < Numbers[i].Length; j++) {
+            //        Console.WriteLine(Numbers[i][j]);
+            //    }
+            //}
+            #endregion
+
+
+
+            #region Functions - By Value
+            //int A = 3;
+            //int B = 5;
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+            //Console.WriteLine($" \n ***********");
+
+            //Swap( A,  B);
+
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+            //Console.WriteLine($" \n ***********");
+
+            #endregion
+
+            #region Functions - By Reference
+
+            //int A = 3;
+            //int B = 5;
+            //Console.WriteLine($"{A} - {A.GetHashCode()}");
+            //Console.WriteLine($"{B} - {B.GetHashCode()}");
+            //Console.WriteLine($" \n ***********");
+
+            //SwapByRef(ref A, ref B);
+
+            //Console.WriteLine($"{A} - {A.GetHashCode()}");
+            //Console.WriteLine($"{B} - {B.GetHashCode()}");
+            //Console.WriteLine($" \n ***********");
+            #endregion
+
+            #region Functions - By Out
+
+            //int A = 3;
+            //int B = 5;
+            //int[] Result = {0,0};
+            //SumMul(A ,B, out Result[0], out Result[1]);
+
+            //Console.WriteLine($"Sum: {Result[0]}");
+            //Console.WriteLine($"Mul: {Result[1]}");
+
+            #endregion
+
+            #region Functions - Params
+            //int[] Nums = {1,2,3};
+            //int Total= SumArray(1,2,3);
+            //Console.WriteLine($"Sum: {Total}");
+            #endregion
+
+            #region Boxing - UnBoxing
+            //Boxing : casting from ValueType to ReferenceType
+            object obj; //Declare For Reference of type "Object", Refrencing to NULL
+            //this refrence [obj] can refer to an instance from type object or any other datatype
+            obj= new object();
+            obj = new string ("Nana") ;
+            obj = "B"; // Casting from object type to [Char] value type // impliceit // Boxing
+            obj = 3; // Casting from Char type to [int] value type // impliceit // Boxing
+
+            //UnBoxing : casting from ReferenceType to ValueType //Unsafe - MayBe throw Exiption
+            object O2 = 10;
+            O2 = "Ahmed";
+            int Y = (int)O2;
+            Console.WriteLine(Y);
+
+
+            #endregion
+        }
+    }
+}
